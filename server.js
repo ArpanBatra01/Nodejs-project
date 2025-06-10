7381822d81780bf8b4e5d926c755f66c2285e8c3
ArpanBatra01 <arpanbatra60@gmail.com>
2025-06-10 16:04:25 +0530

    Project setup

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+
+const app = express();
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, your server is running!');
+});
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+.then(() => console.log("MongoDB connected"))
+
+.catch(err => console.error("MongoDB connection error:", err));
+
+
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
