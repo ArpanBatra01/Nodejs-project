e56965d8763ff564b35a6feb5b515628cd81bbbb
ArpanBatra01 <arpanbatra60@gmail.com>
2025-06-16 19:00:29 +0530

    Creating get & post api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const User = require('../models/user.js');
 
-router.post('/', async (req, res) => {
+
+
+router.post('/users', async (req, res) => {
 
   try {
     const newUser = new User(req.body);
 
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+
   } catch (err) {
 
     res.status(400).json({ message: err.message });
@@ -16,12 +19,13 @@ router.post('/', async (req, res) => {
 });
 
 router.get('/', async (req, res) => {
-    
+
   try {
     const users = await User.find();
     res.json(users);
   } catch (err) {
     res.status(500).json({ message: err.message });
+
   }
 });
 
--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 require('dotenv').config();
 
-
 const app = express();
-
-
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -17,14 +13,15 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-
-.then(() => console.log("MongoDB connected"))
-
-.catch(err => console.error("MongoDB connection error:", err));
-
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
 const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+
+
 app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
